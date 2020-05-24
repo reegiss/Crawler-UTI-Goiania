db41ff67d261d3dba7acc5d9d2c21b4ac7a80f90
Regis Araujo Melo <regis@r3tecnologia.net>
2020-05-24 20:17:01 -0300

    filtrando resultados

--- src/index.js
+++ src/index.js
@@ -5,11 +5,26 @@ const url = "https://www.goiania.go.gov.br/sistemas/siscv/asp/siscvg0930f0.asp";
 fetchData(url).then( (res) => {
     const html = res.data;
     const $ = cheerio.load(html);
-    const statsTable = $('.tabela > tbody > tr');
-    statsTable.each(function() {
-        let title = $(this).find('td').text();
-        console.log(title);
+
+    const result = $(".tabela > tbody > tr").map((i, element) => ({
+        unitDesc: $(element).find('td:nth-of-type(1)').text().trim(),
+        descriptionBed: $(element).find('td:nth-of-type(2)').text().trim(),
+        busyAmount: $(element).find('td:nth-of-type(3)').text().trim(),
+        freeAmount: $(element).find('td:nth-of-type(4)').text().trim(),
+        reservAmount: $(element).find('td:nth-of-type(5)').text().trim(),
+        totalOffered: $(element).find('td:nth-of-type(7)').text().trim()
+    })).get();
+
+    const filterResult = result.filter((item) => {
+        return item.unitDesc != '' &&
+            item.descriptionBed != '' &&
+            item.busyAmount != '' &&
+            item.freeAmount != '' && 
+            item.reservAmount != '' &&
+            item.totalOffered != '';
     });
+    
+    console.log(JSON.stringify(filterResult));
 })
 
 async function fetchData(url){
